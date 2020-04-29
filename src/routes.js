050061f9ed502ee0854f1e371c3ffd87dae41f22
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2020-04-29 13:40:53 -0300

    auth middleware: builded

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,14 @@ import { Router } from 'express';
 import UserConstroller from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserConstroller.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 routes.put('/users', UserConstroller.update);
 
