0cb0b5deaec7361adfe44e2192c64f10b94248aa
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2020-05-03 18:11:26 -0300

    file controller and yours features

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,19 @@
 import { Router } from 'express';
+
+import multer from 'multer';
 import UserConstroller from './app/controllers/UserController';
+import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import ListController from './app/controllers/ListController';
 import TaskController from './app/controllers/TaskController';
+
+import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
 import validatorMiddleware from './app/middlewares/validation';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post(
   '/users',
@@ -27,6 +33,14 @@ routes.put(
   validatorMiddleware.userUpdateValidator,
   UserConstroller.update
 );
+
+routes.post(
+  '/files',
+  upload.single('file'),
+  validatorMiddleware.fileCreateValidator,
+  FileController.store
+);
+
 routes.get(
   '/lists',
   validatorMiddleware.listIndexValidator,
