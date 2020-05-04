f47409b62f6e444ee87b090c3876181958978590
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2020-05-04 12:49:43 -0300

    updates in mongodb setups

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 import 'dotenv/config';
 import express from 'express';
 import { resolve } from 'path';
-import routes from './routes';
+import cors from 'cors';
 
-import './database';
+import routes from './routes';
+import db from './database';
 
 class App {
   constructor() {
     this.server = express();
+    this.database();
     this.middlewares();
     this.routes();
   }
 
+  database() {
+    db.connect();
+  }
+
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
--- src/database/index.js
+++ src/database/index.js
@@ -6,11 +6,32 @@ class Database {
   }
 
   mongo() {
-    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
+    this.connect = async () => {
+      if (mongoose.connection.readyState === 0) {
+        this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+          useNewUrlParser: true,
+          useCreateIndex: true,
+          useFindAndModify: false,
+          useUnifiedTopology: true,
+        });
+      }
+    };
+    this.truncate = async () => {
+      if (mongoose.connection.readyState !== 0) {
+        const { collections } = mongoose.connection;
+
+        const promises = Object.keys(collections).map(collection =>
+          mongoose.connection.collection(collection).deleteMany({})
+        );
+
+        await Promise.all(promises);
+      }
+    };
+    this.disconnect = async () => {
+      if (mongoose.connection.readyState !== 0) {
+        await mongoose.disconnect();
+      }
+    };
   }
 }
 
